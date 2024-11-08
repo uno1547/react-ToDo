0e8a46cafc6454fd3b90ece1a3e74bae73154148
Yuno Kim <unokim1547@gmail.com>
2024-11-09 02:38:14 +0900

    adding new todo as state > component refresh
    fix map component key issue

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,12 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import style from "./ToDo.module.css"
 
 function ToDo({ text, isDone }) {
+  console.log('todo 랜더링');
+  // console.log(text, isDone);
+  const [done, setDone] = useState(isDone)
+  console.log('새로 랜더링된 state값', done);
+  // useEffect(() => {
+  //   setDone(isDone)
+  // }, [isDone])
+  // console.log(`${text}의 props의 isDone`, isDone);
+  // console.log(`${text}의 state`, done);
+  const clickHandler = (evt) => {
+    console.log('clicked!!');
+    setDone(!done)
+    // console.log(evt.currentTarget)
+    // setDone(!done)
+    /*
+    // const checkbox = evt.currentTarget.querySelector("input")
+      클릭하면 체크박스가 체크되게 하고, 이후에 데이터 처리
+      아니면 데이터 처리 먼저하고 랜더링을 다시하면 저절로 체크되있을지도 이게 맞는듯 반영이 되야하니깐
+    */
+  }
   return (
     <>
-      <li className = {style.todo}>
-        <input type = "checkbox" defaultChecked={isDone}></input>
-        <span>{text}</span>
+      <li className = {style.todo} onClick={clickHandler}>
+        <input type = "checkbox" checked = {done} readOnly></input>
+        <span>{text} {done ? "완료" : "미완료"}</span>
         <div>
           <button>수정</button>
           <button>휴지통</button>
--- src/components/ToDos.js
+++ src/components/ToDos.js
@@ -3,22 +3,45 @@ import style from "./ToDos.module.css";
 import ToDo from "./ToDo";
 
 function ToDos() {
+  const [toDo, setToDo] = useState() // 
+  // const [cnt, setCnt] = useState(0)
   const [toDos, setToDos] = useState([
-    { text : "할일1", isDone : false},
-    { text : "할일2", isDone : false},
-    { text : "할일3", isDone : true}
+    // 데이터의 id는 고유하게 정해진다고 가정!! 이건 별도로 처리가 필요할듯함 global에 index를 주거나, 데이터 생성시에 고유값을 설정하던가
+    { id : "첫번째", text : "첫번째", isDone : false},
+    { id : "두번째", text : "두번째", isDone : false},
+    { id : "세번째", text : "세번째", isDone : true}
   ])
-  console.log('todos 랜더링!!');
+
+  const changeHandler = (evt) => {
+    // console.log(evt.target);
+    setToDo({id : evt.target.value, text : evt.target.value, isDone : false}) // 이때 state가 변하긴하지만 아래 콘솔이찍히고 나서야 랜더링이 다시되기때문에 콘솔에선 변한 state를 볼수없는것??
+    // console.log(toDo); // state를 찍는것은 볼수없다?
+  }
+
+  const submitHandler = (evt) => {
+    // console.log('submit');
+    evt.preventDefault()
+    if(!toDo) return
+    // console.log([toDo, ...toDos]);
+    // setToDos(prev => [toDo, ...toDos]) // shift
+    setToDos(prev => [...prev, toDo]) // push
+  }
+  console.log(toDos);
+  // console.log('todos 랜더링!!'); // toDo가 바뀔때마다 랜더링이 다시된다!!
   return (
     <div className={style.todos}>
-      <form className={style.form}>
-        <input placeholder="할일을 입력하세요"></input>
+      <form className={style.form} onSubmit={submitHandler}>
+        <input placeholder="할일을 입력하세요" onChange={changeHandler}></input>
         <button type="submit">추가</button>
       </form>
       <ul>
         {toDos.map((todo, idx) => {
-          return <ToDo text={todo.text} isDone={todo.isDone} key={idx}/>
+          // console.log(`${todo.text}${todo.isDone}${idx}`);
+          return <ToDo text={todo.text} isDone={todo.isDone} key={todo.id}/>
+          // return <ToDo text={todo.text} isDone={todo.isDone} key={todo.text}/>
         })}
+        {
+        }
       </ul>   
     </div>
   )
