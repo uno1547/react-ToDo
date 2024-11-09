38d47ee0b7f8819edafcd3f91204b4a247c730f5
Yuno Kim <unokim1547@gmail.com>
2024-11-09 23:16:58 +0900

    make modal component
    basic markup and style

---
+++ src/components/Modal.js
@@ -0,0 +1,26 @@
+import style from "./Modal.module.css"
+
+function Modal() {
+  const submitHandler = (evt) => {
+    console.log('submit');
+    evt.preventDefault()
+  }
+  return (
+    <div className={style.modal_container}>
+      <div className={style.modal}>
+        <div className={style.modal_header}>
+          <p className={style.close}>&times;</p>
+        </div>
+        <form className={style.modal_content} id="update-form" onSubmit={submitHandler}>
+          <input placeholder="수정사항을 입력하세요" />
+        </form>
+        <div className={style.modal_footer}>
+          <button type="submit" form="update-form" className = {`${style.btn} ${style.btn_submit}`}>수정</button>
+          <button className = {`${style.btn} ${style.btn_cancel}`}>취소</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Modal
